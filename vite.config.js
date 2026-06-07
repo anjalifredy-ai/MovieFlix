d8f402e9c4947f96cbc48d45e1be331756766ccc
anjalifredy-ai <anjalifredy@gmail.com>
2026-06-07 11:44:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api/tmdb': {
-        target: 'https://api.themoviedb.org',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/tmdb/, '/3')
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        }
       }
     }
   }
